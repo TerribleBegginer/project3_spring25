157293bf8856cbf7318c26ef2085dc969f6e7164
TerribleBegginer <domcasias.0420@gmail.com>
2025-04-09 10:53:35 -0600

    Update rsh.c

--- rsh.c
+++ rsh.c
@@ -5,42 +5,91 @@
 #include <unistd.h>
 #include <string.h>
 
+#define MAX_ARGS 21  // 20 arguments + NULL
 #define N 12
 
 extern char **environ;
 
-char *allowed[N] = {"cp","touch","mkdir","ls","pwd","cat","grep","chmod","diff","cd","exit","help"};
+char *allowed[N] = {
+    "cp", "touch", "mkdir", "ls", "pwd", "cat", "grep", "chmod", "diff",
+    "cd", "exit", "help"
+};
 
-int isAllowed(const char*cmd) {
-	// TODO
-	// return 1 if cmd is one of the allowed commands
-	// return 0 otherwise
-	
-	return 0;
+
+int isAllowed(const char *cmd) {
+    for (int i = 0; i < N; i++) {
+        if (strcmp(allowed[i], cmd) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void printHelp() {
+    printf("The allowed commands are:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%d: %s\n", i + 1, allowed[i]);
+    }
 }
 
 int main() {
-
-    // TODO
-    // Add variables as needed
-
     char line[256];
 
     while (1) {
+        fprintf(stderr, "rsh>");
+        if (fgets(line, sizeof(line), stdin) == NULL) continue;
+        if (strcmp(line, "\n") == 0) continue;
 
-	fprintf(stderr,"rsh>");
+        line[strcspn(line, "\n")] = '\0';
 
-	if (fgets(line,256,stdin)==NULL) continue;
+      
+        char *argv[MAX_ARGS];
+        int argc = 0;
+        char *token = strtok(line, " ");
+        while (token != NULL && argc < MAX_ARGS - 1) {
+            argv[argc++] = token;
+            token = strtok(NULL, " ");
+        }
+        argv[argc] = NULL;
 
-	if (strcmp(line,"\n")==0) continue;
+        if (argc == 0) continue;
 
-	line[strlen(line)-1]='\0';
+        int cmdIndex = isAllowed(argv[0]);
 
-	// TODO
-	// Add code to spawn processes for the first 9 commands
-	// And add code to execute cd, exit, help commands
-	// Use the example provided in myspawn.c
+        if (cmdIndex == -1) {
+            printf("NOT ALLOWED!\n");
+            continue;
+        }
 
+     
+        if (strcmp(argv[0], "exit") == 0) {
+            return 0;
+        } else if (strcmp(argv[0], "help") == 0) {
+            printHelp();
+        } else if (strcmp(argv[0], "cd") == 0) {
+            if (argc > 2) {
+                printf("-rsh: cd: too many arguments\n");
+            } else {
+                const char *target = argc == 2 ? argv[1] : getenv("HOME");
+                if (chdir(target) != 0) {
+                    perror("cd failed");
+                }
+            }
+        } else {
+          
+            pid_t pid;
+            int status;
+            posix_spawnattr_t attr;
+
+            posix_spawnattr_init(&attr);
+            if (posix_spawnp(&pid, argv[0], NULL, &attr, argv, environ) != 0) {
+                perror("spawn failed");
+            } else {
+                waitpid(pid, &status, 0);
+            }
+            posix_spawnattr_destroy(&attr);
+        }
     }
+
     return 0;
 }
